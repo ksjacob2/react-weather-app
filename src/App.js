89915f4e46f92621b9143114b47744a64eb29eb0
ksjacob2 <102990304+ksjacob2@users.noreply.github.com>
2024-03-20 14:33:39 -0400

    Added the GitHub repository URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Kate Jacob and is{" "}
+        <a href="https://github.com/ksjacob2/react-weather-app" target="_blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
