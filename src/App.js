2043fdb84b26cc8f0bad358bfb710c35e3088cc1
ksjacob2 <102990304+ksjacob2@users.noreply.github.com>
2024-03-20 23:08:12 -0400

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Kate Jacob and is{" "}
-        <a href="https://github.com/ksjacob2/react-weather-app" target="_blank">
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Kate Jacob and is{" "}
+          <a
+            href="https://github.com/ksjacob2/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
